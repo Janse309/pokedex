f70f499c312022bed76e15612e9b29f8eef92d67
Janse309 <jansimonboecker40@gmail.com>
2026-05-30 20:09:39 +0200

    add css data for background-stylings for different pokemon types

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let BASE_URL = "https://pokeapi.co/api/v2/";
 let ICON_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/legends-arceus/";
 
+console.log(ICON_URL);
+
+
 let allPokemon = [];
 let currentPokemon = [];
 
@@ -30,11 +33,18 @@ function renderSinglePokemon(pokeResponse) {
 function renderTypes(pokeResponse) {
     let typeText = "";
     for (let i = 0; i < pokeResponse.types.length; i++) {
-        let type = pokeResponse.types[i].type.name;
-        typeText += `<span><b>${type} </b></span>`;
+        let typeName = pokeResponse.types[i].type.name;
+        let typeUrl = pokeResponse.types[i].type.url;
+        
+        let urlParts = typeUrl.split('/');
+        let typeId = urlParts[urlParts.length - 2];
+        typeText += `
+            <span>
+                <img src="${ICON_URL}${typeId}.png" alt="${typeName}">
+            </span>`;
     }
     return typeText;
 }
 
-fetchPokemon(23);
+fetchPokemon(1);
 
