5b482747f74b4093393f0d15b0d0d192e1967d56
Janse309 <jansimonboecker40@gmail.com>
2026-06-05 13:45:08 +0200

    style: pokemon card header modified, some line delete in html and give some attributes to h1,h2,h3,p3

--- scripts/template.js
+++ scripts/template.js
@@ -3,29 +3,40 @@ function getPokemonInformationTemplate(pokemon, pokemonBgClass) {
     return `   
         <div class="pokemon-card">
             <div class="pokemon-card-header">
-                <p>#${pokemon.id}</p>
                 <h2>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+                <p>#${pokemon.id}</p>
             </div>
             <div class="pokemon-image-container ${pokemonBgClass}">
-                <button class="pokemon-img-button"><img class="pkmn-img" src="${pokemonImg}" alt="${pokemon.name}"></button>
+                <button onclick="openDialog(${pokemon.id})" class="pokemon-img-button"><img class="pkmn-img" src="${pokemonImg}" alt="${pokemon.name}"></button>
             </div>
             <div class="pkmn-card-footer">${renderTypes(pokemon)}</div>
         </div>
     `
 }
 
+function getPokemonDialogTemplate(pokemon, pokemonBgClass, fontColor) {
+    return `
+        <div class="dialog-container">
+            <div class="dialog-header ${pokemonBgClass}">
+                <div class="name-and-id">
+                    <span>#${pokemon.id}</span>
+                    <h2 class="pokemon-name">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+                    <p>X</p>
+                </div>
+                    <img class="pkmn-img dialog-img" src="${IMG_URL + pokemon.id}.png" alt="${pokemon.name}">
+            </div>
+            <div class="dialog-type-container">
+                <div class="type-gap">${renderTypes(pokemon)}</div>
+            </div>
+            <div class="information-btn-section">
+                <button class="dialog-button"><h3 class="${fontColor}">about</h3></button>
+                <button class="dialog-button"><h3 class="${fontColor}">base stats</h3></button>
+                <button class="dialog-button"><h3 class="${fontColor}">evon chain</h3></button>
+            </div>
+        </div>
+    `
+}
 
-// function getPokemonInformationTemplate(pokeResponse, pokemonBgClass) {
-//     return `
-//         <div class="pokemon-card ${pokemonBgClass}">
-//             <div class="pokemon-header">
-//                 <h2>${pokeResponse.name.toUpperCase()}</h2>
-//                 <img src="${pokeResponse.sprites.other['official-artwork'].front_default}" alt="${pokeResponse.name}">
-//             </div>
-            
-//             <div class="pokemon-types">
-//                 ${renderTypes(pokeResponse)}
-//             </div>
-//         </div>
-//     `;
-// }
+//About
+// base stats
+// evo chain
